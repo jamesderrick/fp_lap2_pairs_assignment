9beaf77df09bc2319b652808ea5c939367c87ae9
Tom Virgo <tomcvirgo@gmail.com>
2021-07-21 11:15:22 +0100

    add duplicate url feature

--- server/server.js
+++ server/server.js
@@ -9,28 +9,29 @@ server.use(express.json());
 const port = process.env.PORT || 3000;
 
 const Post = require('./models/post')
-// Root route
-//server.get('/', (req, res) => res.send('Hello, world!'))
 
-server.get('/', async (req, res) => {
+//Root route
+server.get('/', (req, res) => res.send('Hello, world!'))
+
+
+server.post('/', async (req,res) => {
     try {
-        const posts = await Post.all
-        res.json({posts})
-    } catch(err) {
+        const data = req.body;
+        const post = await Post.create(data)
+        res.status(201).json({post})
+    } catch(err){
         res.status(500).json({err})
     }
 })
 
-server.post('/', async (req,res) => {
-    const data = req.body;
-    const post = await Post.create(data)
-    res.status(201).json({post})
-})
-
 server.get('/:id', async (req,res) => {
-    const id = req.params.id
-    const post = await Post.findById(id)
-    res.status(200).json({post})
+    try {
+        const id = req.params.id
+        const post = await Post.findById(id)
+        res.status(200).json({post})
+    } catch(err){
+        res.status(500).json({err})
+    }
 })
 
 module.exports = server
